569b2078f7edfe53d1674db4e52a53716b344722
matias0621 <132005393+matias0621@users.noreply.github.com>
2026-01-31 16:58:01 -0300

    model and animation of sheep

--- scenes/players/players.gd
+++ scenes/players/players.gd
@@ -3,9 +3,11 @@ extends CharacterBody3D
 
 @export var speed:float = 10
 @export var acceleration:float = 20
-var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
-
 @export var device_id:int = -1
+@export var monster_animation:MonsterAnimation
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var _name_animations:NameAnimationMonster = NameAnimationMonster.new()
+
 
 func _ready() -> void:
 	print(device_id)
@@ -16,33 +18,20 @@ func _process(delta: float) -> void:
 	
 	var input_dir = MultiplayerInput.get_vector(device_id ,"player_one_right", "player_one_left", "player_one_down", "player_one_up")
 	
+	if Input.is_action_just_pressed("ui_accept"):
+		monster_animation.play_animation(_name_animations.shock)
+	
 	if input_dir != Vector2.ZERO:
 		velocity.x = lerp(velocity.x, input_dir.x * speed, acceleration * delta)
 		velocity.z = lerp(velocity.z, input_dir.y * speed ,acceleration * delta)
+		var dir_3d = Vector3(input_dir.x, 0, input_dir.y).normalized()
+		var target_rotation = atan2(dir_3d.x, dir_3d.z) + PI
+		rotation.y = lerp_angle(rotation.y, target_rotation, 8 * delta) 
+		
+		monster_animation.play_animation(_name_animations.run)
 	else:
 		velocity.x = 0
 		velocity.z = 0
-	
-	move_and_slide()
-
-
-func _test():
-	var dir:Vector3 = Vector3.ZERO
-	
-	if Input.get_action_strength("player_one_right", device_id) > 0:
-		dir.x += 1
-	
-	if Input.get_action_strength("player_one_left", device_id) > 0:
-		dir.x -= 1
-	
-	if Input.get_action_strength("player_one_down", device_id) > 0:
-		dir.z += 1
-	
-	if Input.get_action_strength("player_one_up", device_id) > 0:
-		dir.z -= 1
-	
-	var dir_normal:Vector3 = dir.normalized()
-	velocity.x = dir_normal.x * speed
-	velocity.z = dir_normal.z * speed 
+		monster_animation.play_animation(_name_animations.idle)
 	
 	move_and_slide()
